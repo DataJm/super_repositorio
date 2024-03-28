0dad47e19be827525bad6d8d85e716cfd876731e
JM <ecojmbarrera@gmail.com>
2024-03-28 17:00:35 -0600

    Agregue archivo inicial
    Este es mi primer arhcivo

---
+++ archivo.py
@@ -0,0 +1,16 @@
+print("hola jose")
+print("hola hola")
+variable =  10 + 5
+
+
+for x in range(3):
+    print(variable)
+
+
+# Esto es un comentario
+# esto es otro comentario
+
+
+# Fin del archivo
+
+print("adios")
